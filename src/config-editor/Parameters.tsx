f209a52b37bb01d7ce44a597e8a002b8e1fcd11d
Cyber MacGeddon <cybermaggedon@gmail.com>
2024-09-01 00:04:21 +0100

    Clipboard copying

--- src/config-editor/Parameters.tsx
+++ src/config-editor/Parameters.tsx
@@ -1,6 +1,9 @@
 
 import React from 'react';
 
+import Button from '@mui/material/Button';
+import { ContentPaste } from '@mui/icons-material';
+
 import ItemParameters from './ItemParameters';
 import { Pattern, ParameterSet } from './Pattern';
 
@@ -15,11 +18,22 @@ const Parameters : React.FC<ParametersProps> =
     ({ selection, deployment, parameters, setParameters }) =>
 {    
 
+    const copyToClipboard = () => {
+        if (deployment) {
+            navigator.clipboard.writeText(deployment);
+        }
+    }
+
     if (deployment)
         return (
             <div className="card deployment-config">
                 <h2>Deployment configuration</h2>
                 <textarea value={deployment} readOnly={true}/>
+                <Button onClick={copyToClipboard} variant="outlined"
+                    startIcon={<ContentPaste/>}
+                >
+                    Copy to clipboard
+                </Button>
             </div>
         );
 
